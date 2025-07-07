8cbd431a16344cd55434b1d3f120602a796e0980
Miguel Sá <miguel.srv@gmail.com>
2025-07-07 11:05:11 +0100

    Updated some dotfiles

--- zsh/.zsh_old/.p10k.colours.onedarkpro.zsh
+++
@@ -1,8 +0,0 @@
-# OneDarkPro Colours 
-local grey='245'
-local red='180'
-local yellow='179'
-local blue='75'                                                                                                                                                                
-local magenta='176'
-local cyan='74'
-local white='145'
